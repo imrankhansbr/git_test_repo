dc13bf29deb66cac13c3dceaa5e3d14facf6a29c
62rjason <62rjason@gmail.com>
2022-09-20 22:41:48 +0530

    code fixed by rakesh

--- prime.js
+++ prime.js
@@ -1,11 +1,19 @@
 function checkprime(num){
     
-let f=0;
+let factor=0;
 for(let i=1;i<=num;i++){
 if(num%i==0){
-f++;
+factor++;
 }}
 if(f==2){
-
+return true;
+}else{
+    return false;
 }
+}
+let answer=checkprime(13);
+if(answer==true){
+    console.log("Prime");
+}else{
+    console.log("Not Prime");
 }
