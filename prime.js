0d8469d0b3038819afc20822b179d2c927c033ba
imrankhansbr <113512202+imrankhansbr@users.noreply.github.com>
2022-09-20 22:28:57 +0530

    Update prime.js

--- prime.js
+++ prime.js
@@ -1,6 +1,13 @@
-let num=13;
+function checkprime(num){
+    
 let f=0;
 for(let i=1;i<=num;i++){
 if(num%i==0){
 f++;
 }}
+if(f==2){
+    console.log("Prime");
+}else{
+    console.log("Not Prime");
+}
+}
